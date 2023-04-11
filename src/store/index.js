c2c5d99eff7f61d31b58d2f06e242f32428e9bdf
Valen <valen.araya@gmail.com>
2023-04-11 13:17:13 -0400

    Rama Valen. Agrega total

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,8 @@ import { createStore } from 'vuex'
 export default createStore({
 
     state: {
-        carrito: []
+        carrito: [],
+        valores: 0
     },
 
     mutations: {
@@ -14,8 +15,10 @@ export default createStore({
             })
             if(yaExiste){
                 payload.cantidad = payload.cantidad + 1 
+                state.valores = state.valores+(payload.precio) 
             }else{
                 state.carrito.push(payload) 
+                state.valores = state.valores+(payload.precio) 
             }
 
             // // let buscaEnCarro = state.carrito.map( e=>e.id)
