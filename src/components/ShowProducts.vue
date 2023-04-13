2ff49125070cfb400a26520987b906c905f3a0cb
RPeque <rodrigo.pequeno.24@gmail.com>
2023-04-13 09:13:48 -0400

    stock y prueba modal

--- src/components/ShowProducts.vue
+++ src/components/ShowProducts.vue
@@ -1,4 +1,5 @@
 <template>
+  
   <div class="text-center container py-5">
     <h4 class="mt-4 mb-5"><strong>Productos</strong></h4>
     <div class="barraB">
@@ -8,12 +9,37 @@
     <div class="row">
       <div class="col-lg-4 col-md-12 mb-4" v-for="producto in disponibles" :key="producto.id">
         <div class="card">
+          <!-- lanzador de modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+  <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top">
+  Tooltip on top
+</button>
+  <!-- lanzador de modal -->
           <button class = "btn btn-success"  @click="agregar(producto)">Agregar</button>
           <img :src="producto.imagen" class="w-100" />
           <div class="card-body">
               <h5 class="card-title mb-3">{{producto.nombre}}</h5>
               <p>{{producto.descripcion}}</p>
             <h6 class="mb-3">${{producto.precio}}</h6>
+          </div>
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -25,6 +51,8 @@
 <script >
 import {ProductService} from "../services/ProductService"
 import {mapState, mapMutations} from 'vuex'
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 export default {
   data: function(){
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from "./store";
 // Bootstrap CSS , JS
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
 
 
 createApp(App).use(router).use(store).mount('#app')
