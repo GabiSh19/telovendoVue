483ab157c2960eca2ea8841a16fbfa5606a0dd17
RochCX <103058028+RochCX@users.noreply.github.com>
2023-04-13 10:32:02 -0400

    Peque2 (#8)
    * arreglos menores y stock funciona card invisible
    
    * stock y prueba modal
    
    * modal agregado

--- src/components/ShowProducts.vue
+++ src/components/ShowProducts.vue
@@ -1,4 +1,5 @@
 <template>
+  
   <div class="text-center container py-5">
     <h4 class="mt-4 mb-5"><strong>Productos</strong></h4>
     <div class="barraB">
@@ -6,15 +7,34 @@
       <button class="button btn-success" v-on:click="searchData">Buscar</button>
     </div>
     <div class="row">
-      <div class="col-lg-4 col-md-12 mb-4" v-for="producto in productos" :key="producto.id">
-        <div class="card" >
+      <div class="col-lg-4 col-md-12 mb-4" v-for="producto in disponibles" :key="producto.id">
+        <div class="card">
           <button class = "btn btn-success"  @click="agregar(producto)">Agregar</button>
-          <img :src="producto.imagen" class="w-100" />
+          <img :src="producto.imagen" class="w-100" data-bs-toggle="modal" :data-bs-target="'#myModal' + producto.id"/>
           <div class="card-body">
               <h5 class="card-title mb-3">{{producto.nombre}}</h5>
               <p>{{producto.descripcion}}</p>
             <h6 class="mb-3">${{producto.precio}}</h6>
           </div>
+        <!-- Ventana modal -->
+          <div class="modal fade" :id="'myModal' + producto.id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">{{producto.nombre}}</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  {{producto.descripcion}}
+                </div>
+                <!-- <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          <!-- Ventana modal -->
         </div>
       </div>
     </div>
@@ -23,8 +43,13 @@
 
 
 <script >
+// instale popper con el comandoo $npm i @popperjs/core
 import {ProductService} from "../services/ProductService"
 import {mapState, mapMutations} from 'vuex'
+
+// import { createPopper } from '@popperjs/core';
+
+
 
 export default {
   data: function(){
@@ -32,7 +57,9 @@ export default {
         productos: [],
         productosTodos: [],
         arrayCarrito:  [],
+        disponibles: [],
         buscarP: '',
+        // checkStock: true,
         cargando: true,
     };
   },
@@ -42,7 +69,9 @@ export default {
       let response = await ProductService.ProductAll();
       this.productos = response;
       this.productosTodos = response;
+      this.disponibles = this.productos.filter((prod) => prod.stock > 0);
       this.cargando = false;
+      
   
     } catch (error) {
       this.errorMessage = error;
@@ -63,7 +92,16 @@ export default {
     },
     reseteoP(){
       this.productos = this.productosTodos
-    }
+    },
+
+    // checkStock(stock) {
+    //   if(stock > 0){
+    //     return true;
+    //   }
+    //   else{
+    //     return false;
+    //   }
+    // }
     // searchData() {
     //   this.productos.filter(
     //     prod =>{
--- src/main.js
+++ src/main.js
@@ -9,4 +9,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 
+
+
+
 createApp(App).use(router).use(store).mount('#app')
