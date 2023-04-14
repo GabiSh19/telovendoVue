17fa05219a67710edc7c87192b1c17eae78282bf
RPeque <rodrigo.pequeno.24@gmail.com>
2023-04-14 10:17:54 -0400

    modal

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,16 +10,35 @@
           </a>
         </div>
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <form class="d-flex" role="search">
+            <li class="nav-item">
               <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
               <button class="btn btn-outline-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Carrito</button>
-            </form>
-            <li class="nav-item" style="margin-right: 20px;">
-              <router-link to="/LoginPage" class="nav-link active">Logout</router-link>
+            </li>
+            <li class="nav-item" style="margin-left: 15px;">
+              <button class="btn btn-outline-dark" type="button" data-bs-toggle="modal" data-bs-target="#loggoutModal">Logout</button>
             </li>
           </ul>
         </div>
     </nav>
+    <div class="modal fade" id="loggoutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel"> Ya te vas?</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  Seguro quieres cerrar sesion?
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                    <router-link to="/" class="nav-link active">Logout</router-link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
   </template>
   
 
